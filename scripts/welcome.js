02bc24cfe9163496f1ab186627dd21a0f7796114
Raphael Heinz <raphael.heinz@gmx.de>
2025-04-01 00:11:19 +0200

    Synchronize plugin and repo name

--- scripts/welcome.js
+++ scripts/welcome.js
@@ -74,7 +74,7 @@ const createRepo = (token, name) => {
     private: true,
     auto_init: true,
     description:
-      'Collection of LeetCode questions to ace the coding interview! - Created using [LeetHub v3](https://github.com/raphaelheinz/LeetHub-3.0)',
+      'Collection of LeetCode questions to ace the coding interview! - Created using [LeetHub-3.0](https://github.com/raphaelheinz/LeetHub-3.0)',
   };
   data = JSON.stringify(data);
 
