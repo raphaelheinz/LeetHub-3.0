a21594950756f4fac222b6dfe83b5041d11c260a
Karthik1144 <gkarthik8461@gmail.com>
2026-07-01 14:23:19 +0530

    Implement getNotesIfAny to sync LeetCode notes to NOTES.md

--- src/js/leetcode.js
+++ src/js/leetcode.js
@@ -1216,7 +1216,35 @@ LeetCodeV2.prototype.getLanguage = function () {
   return '';
 };
 
-LeetCodeV2.prototype.getNotesIfAny = function () {};
+LeetCodeV2.prototype.getNotesIfAny = async function () {
+  const titleSlug = this.submissionData?.question?.titleSlug;
+  if (!titleSlug) return '';
+
+  const noteQuery = {
+    query:
+      '\n    query questionNote($titleSlug: String!) {\n  question(titleSlug: $titleSlug) {\n    questionId\n    note\n  }\n}\n    ',
+    variables: { titleSlug },
+    operationName: 'questionNote',
+  };
+
+  const noteOptions = {
+    method: 'POST',
+    headers: {
+      cookie: document.cookie,
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify(noteQuery),
+  };
+
+  try {
+    const res = await fetch(`${getLeetCodeBaseUrl()}/graphql/`, noteOptions);
+    const data = await res.json();
+    return data?.data?.question?.note || '';
+  } catch (err) {
+    console.log('LeetHub: Error fetching notes', err);
+    return '';
+  }
+};
 
 LeetCodeV2.prototype.extractQuestionNumber = function () {
   return this.submissionData.question.questionFrontendId ?? this.submissionData.question.questionId;
@@ -1516,7 +1544,7 @@ const loader = (leetCode, suffix) => {
       });
 
       /* Upload Notes if any*/
-      let notes = leetCode.getNotesIfAny();
+      let notes = await leetCode.getNotesIfAny();
       let updateNotes;
       if (notes != undefined && notes.length > 0) {
         updateNotes = uploadGit(
